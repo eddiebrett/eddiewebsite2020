e3a91dcbf34de82c1d166f444d3cb9d62c1b2705
eddiebrett <eddiebrett@hotmail.co.uk>
2020-02-26 11:53:03 +0000

    added models to admin panel

--- articles/models.py
+++ articles/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    #add in thumb nail later
+    # add in thumb nail later
 
-
+    def __str__(self):
+        return self.title
