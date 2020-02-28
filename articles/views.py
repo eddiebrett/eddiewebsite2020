9aa4bcd388479e7e46f029b1067ed9f72e04357d
eddiebrett <eddiebrett@hotmail.co.uk>
2020-02-28 18:04:18 +0000

    added styles to article

--- articles/views.py
+++ articles/views.py
@@ -9,7 +9,8 @@ def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/article_list.html', {'articles': articles})
 
+
 def article_detail(request, slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
-    return render(request,"articles/article_detail.html", {"article":article})
+    return render(request, "articles/article_detail.html", {"article": article})
