53465eddcea40b7d12ebb4bdf45fb3191308a44b
eddiebrett <eddiebrett@hotmail.co.uk>
2020-04-14 22:36:48 +0000

    change payment

--- checkout/forms.py
+++ checkout/forms.py
@@ -19,6 +19,5 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = (
-            'full_name', 'phone_number', 'country', 'postcode',
-            'town_or_city', 'street_address1', 'street_address2',
+            'full_name', 'phone_number', 'email', 'street_address1', 'street_address2', 'town_or_city', 'country', 'postcode'        
         )
